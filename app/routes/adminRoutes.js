26163954299063dbf803d417be266cb89d05aacc
Lovel Bangura <abangura9@gmail.com>
2020-04-14 13:28:56 +0000

    included isAuthentication, and uncomment user info

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -16,57 +16,6 @@ router.use(cors());
 // controllers
 const adminController = require('../controllers/adminControllers');
 
-// models
-// var adminModel = require('../models/admin_model');
-
-// passport middleware
-// var passport = require('passport');
-// var LocalStrategy = require('passport-local').Strategy;
-
-// passport.use(new LocalStrategy({
-//     usernameField: 'email',
-//     passReqToCallback: true
-// }, (req, email, password, done) => {
-//     adminModel.findOne({
-//             email: email
-//         })
-//         .then(adminUser => {
-//             // if user not exist
-//             if (!adminUser) {
-//                 return done(null, false, req.flash('error_msg', 'Email not found'));
-//             }
-
-//             // if their is a user
-//             bcrypt.compare(password, adminUser.password, (err, isMatch) => {
-//                 if (err) {
-//                     return err
-//                 }
-
-//                 // if password not match
-//                 if (!isMatch) {
-//                     return done(null, false, req.flash('error_msg', 'Invalid username or Password'));
-//                 }
-
-//                 // if the is found and valid
-//                 return done(null, adminUser, req.flash('success_msg', 'Login Successful'));
-//             });
-
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         });
-// }));
-
-// passport.serializeUser((adminUser, done) => {
-//     done(null, adminUser.id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//     adminModel.findById(id, function(err, adminUser) {
-//         done(err, adminUser);
-//     });
-// });
-
 // admin get request
 router.route('/')
     .get(adminController.index)
@@ -84,71 +33,71 @@ router.route('/')
 
 // admin registration request
 router.route('/register/admin')
-    .get(adminController.adminRegFormGet)
-    .post(adminController.adminRegFormPost);
+    .get(isUserAuthenticated, adminController.adminRegFormGet)
+    .post(isUserAuthenticated, adminController.adminRegFormPost);
 
 // admin homepage route
 router.route('/dashboard')
-    .get(adminController.getDashboard);
+    .get(isUserAuthenticated, adminController.getDashboard);
 
 // admin create posts route
 router.route('/logout')
-    .get(adminController.getLogout);
+    .get(isUserAuthenticated, adminController.getLogout);
 
 // admin marketDataTable route
 router.route('/market')
-    .get(adminController.getMarketDataTable); // 
+    .get(isUserAuthenticated, adminController.getMarketDataTable); // 
 
 // export flow route
 router.route('/exportFlow')
-    .get(adminController.exportFlowGet);
+    .get(isUserAuthenticated, adminController.exportFlowGet);
 
 // admin tradeFlowDataTable route
 router.route('/tradeFlow')
-    .get(adminController.getTradeFlowDataTable); // 
+    .get(isUserAuthenticated, adminController.getTradeFlowDataTable); // 
 
 // admin stockLevelDataTable route
 router.route('/stockLevel')
-    .get(adminController.getStockLevelDataTable);
+    .get(isUserAuthenticated, adminController.getStockLevelDataTable);
 
 // enumerator registration route
 router.route('/register/enumerator')
-    .get(adminController.enumeratorRegFormGet)
-    .post(adminController.enumeratorRegFormPost);
+    .get(isUserAuthenticated, adminController.enumeratorRegFormGet)
+    .post(isUserAuthenticated, adminController.enumeratorRegFormPost);
 
 // enumerator get router
 router.route('/records/enumerators')
-    .get(adminController.enumeratorsRecordsGet);
+    .get(isUserAuthenticated, adminController.enumeratorsRecordsGet);
 
 // enumerator delete route
 router.route('/records/enumerators/:id')
-    .get(adminController.enumeratorsRecordsDelete);
+    .get(isUserAuthenticated, adminController.enumeratorsRecordsDelete);
 
 // enumerator edit routes
 router.route('/records/enumerator/edit/:id')
-    .get(adminController.enumeratorEditRecordGet)
-    .post(adminController.enumeratorUpdateRecordPost);
+    .get(isUserAuthenticated, adminController.enumeratorEditRecordGet)
+    .post(isUserAuthenticated, adminController.enumeratorUpdateRecordPost);
 
 // farmer registration route
 router.route('/register/farmer')
-    .get(adminController.farmerRegFormGet)
-    .post(adminController.farmerRegFormPost);
+    .get(isUserAuthenticated, adminController.farmerRegFormGet)
+    .post(isUserAuthenticated, adminController.farmerRegFormPost);
 
 // creat market data route
 router.route('/createMarketData')
-    .get(adminController.marketDataGet) // 
+    .get(isUserAuthenticated, adminController.marketDataGet) // 
 
 // wholesale post routess
 router.route('/create_wholesale')
-    .post(adminController.wholesaleDataPost);
+    .post(isUserAuthenticated, adminController.wholesaleDataPost);
 
 // retail post routess
 router.route('/create_retail')
-    .post(adminController.retailDataPost);
+    .post(isUserAuthenticated, adminController.retailDataPost);
 
 // farmgate post routess
 router.route('/create_farm_gate')
-    .post(adminController.farmGateDataPost);
+    .post(isUserAuthenticated, adminController.farmGateDataPost);
 
 // creat farmer data route
 // router.route('/farmerDisData')
@@ -157,54 +106,54 @@ router.route('/create_farm_gate')
 
 // post request product view route
 router.route('/post/new/product')
-    .get(adminController.availableProductFormGet);
+    .get(isUserAuthenticated, adminController.availableProductFormGet);
 
 // post request product by district data route
 router.route('/post/new/product/district')
-    .post(adminController.availableProductByDistFormPost);
+    .post(isUserAuthenticated, adminController.availableProductByDistFormPost);
 
 // post request available-for-sale product by district data route
 router.route('/post/new/product/available')
-    .post(adminController.availableProductForSaleFormPost);
+    .post(isUserAuthenticated, adminController.availableProductForSaleFormPost);
 
 // delete an order route
 router.route('/order/delete/:id')
-    .get(adminController.deleteOrder);
+    .get(isUserAuthenticated, adminController.deleteOrder);
 
 // fbo get router
 router.route('/records/fbos')
-    .get(adminController.fbosRecordsGet);
+    .get(isUserAuthenticated, adminController.fbosRecordsGet);
 
 // fbo delete route
 router.route('/records/fbos/:id')
-    .get(adminController.fbosRecordsDelete);
+    .get(isUserAuthenticated, adminController.fbosRecordsDelete);
 
 // fbo edit route
 router.route('/fbo/record/edit/:id')
-    .get(adminController.fbosRecordEditGet)
-    .put(adminController.fboUpdateRecordPost);
+    .get(isUserAuthenticated, adminController.fbosRecordEditGet)
+    .put(isUserAuthenticated, adminController.fboUpdateRecordPost);
 
 // about get and post routes
 router.route('/about')
-    .get(adminController.aboutGet);
+    .get(isUserAuthenticated, adminController.aboutGet);
 
 // about edit post and get routes
 router.route('/about/edit/:id')
-    .get(adminController.aboutEditGet)
-    .post(adminController.aboutEditPost);
+    .get(isUserAuthenticated, adminController.aboutEditGet)
+    .post(isUserAuthenticated, adminController.aboutEditPost);
 
 // learning routes
 router.route('/learning-info')
-    .get(adminController.learningGet)
-    .post(adminController.learningPost);
+    .get(isUserAuthenticated, adminController.learningGet)
+    .post(isUserAuthenticated, adminController.learningPost);
 
 // learning delete route
 router.route('/learning-info/delete/:id')
-    .get(adminController.learningDelete);
+    .get(isUserAuthenticated, adminController.learningDelete);
 
 // learning edit get route
 router.route('/learning-info/edit/:id')
-    .get(adminController.learningEditRecordGet)
-    .post(adminController.learningUpdateRecordPost);
+    .get(isUserAuthenticated, adminController.learningEditRecordGet)
+    .post(isUserAuthenticated, adminController.learningUpdateRecordPost);
 
 module.exports = router;
